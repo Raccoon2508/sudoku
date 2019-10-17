91f75c468700f2c868fa544db3845030f6451b55
Mikita <45191288+Raccoon2508@users.noreply.github.com>
2019-10-17 14:45:42 +0300

    Rebuild with recursion

--- src/index.js
+++ src/index.js
@@ -1,143 +1,36 @@
 module.exports = function solveSudoku(matrix){
-
- 
   
-  let counter;
-  let resultArr=matrix;
-  
-   Array.prototype.diff = function(a) {
+Array.prototype.diff = function(a) {
     return this.filter(function(i) {return a.indexOf(i) < 0;});
   };  
   Array.prototype.analog = function(a) {
     return this.filter(function(i) {return a.indexOf(i) >= 0;});
-  };
-  
-  
- //do{
-  counter=0;
-  
-  
-  
-  let squareArray=[[],[],[],[],[],[],[],[],[]];//массив квадратиков
-  let squareArrayZeros=[[],[],[],[],[],[],[],[],[]];//массив нулей квадратиков
-  let rowArray=[[],[],[],[],[],[],[],[],[]];//массив строк
-  let rowArrayZeros=[[],[],[],[],[],[],[],[],[]];//массив нулей строк
-  let columnArray=[[],[],[],[],[],[],[],[],[]];//массив столбцов
-  let columnArrayZeros=[[],[],[],[],[],[],[],[],[]];//массив нулей в колонках
-  let etalonArray=[1,2,3,4,5,6,7,8,9];//эталонный массив
-  
-  function count(a,i){ //счетчик количества значений в массиве
-   var result = 0;
-   for(var o in a)
-    if(a[o] == i)
-     result++;
-   
-   return result;
-  }  
-  
-  function fillZeros(arr){
-    for(let i=0; i<9; i++){
-    for(let j=0; j<9; j++){
-      
-      arr[j].push(0);
-      
-     
-  }
-  }
-    
-}
-  
- 
-   
-  fillZeros(squareArray);
-  fillZeros(rowArray);
-  fillZeros(columnArray);
-  fillZeros(rowArrayZeros);
-  fillZeros(columnArrayZeros);
-  
-  
-  
-  //i- ряд, j-столбец
-  for(let i=0; i<9; i++){ 
-    for(let j=0; j<9; j++){
-      
-      squareArray[squareNumber(i,j)][(resultArr[i][j])-1]=resultArr[i][j];
-      
-     
-  }
-  }
-  
- 
-  
-  for(let i=0; i<9; i++){//предполагаемые нули для квадратов, Работает ОК!
-    for(let j=0; j<9; j++){
-      if(squareArray[i][j]==0){
-        
-        squareArrayZeros[i].push(j+1);
+  };   
+
+
+
+let potentZeros=[];
+console.log(zeroCell(matrix))
+
+if(zeroCell(matrix)){
+let zeroRow=zeroCell(matrix)[0];
+console.log(zeroCell(matrix)[0])
+console.log(zeroCell(matrix)[1])
+
+let zeroCol=zeroCell(matrix)[1];
+potentZeros=potencialValues(zeroRow,zeroCol,matrix);
+console.log(potentZeros);
+}else{
+return matrix;}
+  if(potentZeros.length>0){
+    for(let i=0; i<potentZeros.length; i++){
+      potentZeros[i];
+      solveSudoku(matrix);
       }
-    }
-  }
-        
-  
-        
-  
-  
-  
-  for(let i=0; i<9; i++){     //массив строк
-    for(let j=0; j<9; j++){
-      rowArray[i][j]=resultArr[i][j];}
-  }  
-  
-  for(let i=0; i<9; i++){
-   rowArrayZeros[i]=etalonArray.diff(rowArray[i]); 
-  } 
-  
-  
-  
-  
-  //i-строка, j-столбец
-  
-   
-  
-  
-  for(let i=0; i<9; i++){     //массив массив столбцов
-    for(let j=0; j<9; j++){
-     columnArray[j][i]=resultArr[i][j];
-        
-  }  
-  }
-  
-  for(let i=0; i<9; i++){
-    columnArrayZeros[i]=etalonArray.diff(columnArray[i]); 
-    
-    }  
-  
-  
-  
-  
- 
-
-  
- 
- for(let i=0; i<9; i++){
-    for(let j=0; j<9; j++){
-      if(resultArr[i][j]==0){
-            resultArr[i][j]=compareZeros(i,j);
-            counter++;
-            ;
-            if(resultArr[i][j]===0){continue;}else{
-              console.log(resultArr);
-              solveSudoku(resultArr);
-              
-              }
-             
-          
-        }
-      } 
-    }
-  
-
-
+     }
+     
+     
+console.log(matrix);     
 
 function potencialValues(row,col,arr){
     let rowZeros=[];
@@ -169,20 +62,26 @@ function potencialValues(row,col,arr){
   squareZeros=etalonArray.diff(square);
   console.log(squareZeros);
   
+  function squareNumber(a,b){
+  return Math.floor(b/3)+Math.floor(a/3)*3;
+  }
+  
+  
   
   return rowZeros.analog(columnZeros).analog(squareZeros);
   
   
-  function squareNumber(a,b){
-  return Math.floor(b/3)+Math.floor(a/3)*3;
-  } 
+}  
   
+  
+  function zeroCell(arr){
+  for(let i=0; i<9; i++){ 
+    for(let j=0; j<9; j++){
+      if(arr[i][j]==0){return [i,j];}
+    }
+  }
+}
+  
+ 
    
 }
-
-
-let result=solveSudoku(initial);
-
-  
-}
-  
