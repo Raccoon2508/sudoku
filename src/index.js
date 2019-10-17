9d3623ab6d83f8ecec54677b387d3cf1a4615bd3
Mikita <45191288+Raccoon2508@users.noreply.github.com>
2019-10-17 13:23:32 +0300

    Rebuild solution

--- src/index.js
+++ src/index.js
@@ -139,78 +139,45 @@ module.exports = function solveSudoku(matrix){
 
 
 
-function compareZeros(i,k){
-  let square=squareArrayZeros[squareNumber(i,k)];
-  let resOfCompZeros=rowArrayZeros[i].analog(columnArrayZeros[k].analog(squareArrayZeros[squareNumber(i,k)]));  
-  
-  
-  if(resOfCompZeros.length===1){return resOfCompZeros[0]; 
-    }else{
-      return 0;
-}  
-
-}
-
-
-  
- 
-  
-  
- /* function compareZeros(i,j){
-    variantsOfZerozFromZeroz=[];
+function potencialValues(row,col,arr){
+    let rowZeros=[];
+    let etalonArray=[1,2,3,4,5,6,7,8,9];
+    let columnArr=[];
+    let columnZeros=[];
+    let square=[];
+    let squareZeros=[];
     
-    for(p=0; p<compareArrays(i,j).length; p++){
-      if(compareArrays(i,j).length>1
-      &&count(squareArrayZeros[i],compareArrays(i,j)[p])==1
-      ||count(rowArrayZeros[i],compareArrays(i,j)[p])==1
-      ||count(columnArrayZeros[j],compareArrays(i,j)[p])==1)
-      {
-        
-        variantsOfZerozFromZeroz.push(compareArrays(i,j)[p]);   
-        
-        }
-     
-  } 
-  if(variantsOfZerozFromZeroz.length==1){
-    return variantsOfZerozFromZeroz[0]; 
-    }else{
-    return 0;    
-    }   
+    rowZeros=etalonArray.diff(arr[row]);
+    console.log(rowZeros)
+    
+    for(let i=0; i<9; i++){     //массив массив столбцов
+    {
+     columnArr[i]=arr[i][col];
+    }  
   }
-  */
+  
+  columnZeros=etalonArray.diff(columnArr);
+  console.log(columnZeros);
+  
+  for(let i=0; i<9; i++){ 
+    for(let j=0; j<9; j++){
+      if(squareNumber(row,col)==squareNumber(i,j))
+    square.push(arr[i][j]);
+  }
+  }
+  
+  squareZeros=etalonArray.diff(square);
+  console.log(squareZeros);
   
   
-    
-    
-  function compareArrays(i,j){
-    let variantsOfZeroz=[];
-    let variantsOfZerozFromZeros=[];
-    for(p=0; p<9; p++){
-      let etalon=etalonArray[p];
-     
-      if(squareArray[squareNumber(i,j)][p]!=etalon
-      &&rowArray[i][p]!=etalon
-      &&columnArray[j][p]!=etalon){
-        variantsOfZeroz.push(etalonArray[p]);
-             
-        }
-      }
-            
-  return variantsOfZeroz;      
-  } 
-      
+  return rowZeros.analog(columnZeros).analog(squareZeros);
+  
   
   function squareNumber(a,b){
-    let index;
-    index=Math.floor(b/3)+Math.floor(a/3)*3;
-    return index;
-    
+  return Math.floor(b/3)+Math.floor(a/3)*3;
   } 
   
-  
-  //}while(counter>0)  
-  
-  
+   
 }
 
 
