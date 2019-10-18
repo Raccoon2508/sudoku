999ad892e8e9d5360e508385b3b190ebfa68706c
Raccoon2508 <ridge2006@mail.ru>
2019-10-18 16:16:43 +0300

    Final solution

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ let potentZeros=[];
 let zeroRow;
 let zeroCol;
 
-
 if(zeroCell(matrix)){
 zeroRow=zeroCell(matrix)[0];
 zeroCol=zeroCell(matrix)[1];
@@ -14,14 +13,17 @@ potentZeros=potencialValues(zeroRow,zeroCol,matrix);
 }else{return matrix;}
  
  
- if(potentZeros.length>0){
-    for (let i=0;i<potentZeros.length; i++){
-      matrix[zeroRow][zeroCol]=potentZeros[i];
-      solveSudoku(matrix);
-      if(!zeroCell(matrix)){return matrix;}
-      }
-    }   
-  }
+if(potentZeros.length>0){
+ for (let i=0;i<potentZeros.length; i++){
+   matrix[zeroRow][zeroCol]=potentZeros[i];
+   solveSudoku(matrix);
+   if(zeroCell(matrix)==false){
+     return matrix;}
+    }
+  matrix[zeroRow][zeroCol]=0;
+ }   
+}
+
 function potencialValues(row,col,arr){
   
   Array.prototype.diff = function(a) {
@@ -53,7 +55,9 @@ function potencialValues(row,col,arr){
   function squareNumber(a,b){return Math.floor(b/3)+Math.floor(a/3)*3;} //номер квадрата, проверен
   
   return rowZeros.analog(columnZeros).analog(squareZeros);//поиск совпадений в массиве нулей рядов, столбцов и квадратов, проверен
-  function zeroCell(arr){//поиск нулевой ячейки
+  
+  
+function zeroCell(arr){
   for(let i=0; i<9; i++){ 
     for(let j=0; j<9; j++){
       if(arr[i][j]==0){return [i,j];}
@@ -61,6 +65,4 @@ function potencialValues(row,col,arr){
   }
   return false;
 }
-  
-  
 }    
