8546fc90f22538024d0cf61fdcbbabdba4dd7ccb
Raccoon2508 <ridge2006@mail.ru>
2019-10-18 18:19:41 +0300

    Final solution test passed

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ let potentZeros=[];
 let zeroRow;
 let zeroCol;
 
+
 if(zeroCell(matrix)){
 zeroRow=zeroCell(matrix)[0];
 zeroCol=zeroCell(matrix)[1];
@@ -13,50 +14,18 @@ potentZeros=potencialValues(zeroRow,zeroCol,matrix);
 }else{return matrix;}
  
  
-if(potentZeros.length>0){
- for (let i=0;i<potentZeros.length; i++){
-   matrix[zeroRow][zeroCol]=potentZeros[i];
-   solveSudoku(matrix);
-   if(zeroCell(matrix)==false){
-     return matrix;}
-    }
-  matrix[zeroRow][zeroCol]=0;
- }   
+ if(potentZeros.length>0){
+    for (let i=0;i<potentZeros.length; i++){
+      matrix[zeroRow][zeroCol]=potentZeros[i];
+      solveSudoku(matrix);
+      if(!zeroCell(matrix)){return matrix;}
+      }
+      matrix[zeroRow][zeroCol]=0;
+     }   
+   
 }
 
-function potencialValues(row,col,arr){
-  
-  Array.prototype.diff = function(a) {
-    return this.filter(function(i) {return a.indexOf(i) < 0;});
-  };  
-  Array.prototype.analog = function(a) {
-    return this.filter(function(i) {return a.indexOf(i) >= 0;});
-  };   
-    let rowZeros=[];
-    let etalonArray=[1,2,3,4,5,6,7,8,9];//эталонный массив
-    let columnArr=[];
-    let columnZeros=[];
-    let square=[];
-    let squareZeros=[];
-    
-    rowZeros=etalonArray.diff(arr[row]);//массив нулей рядов, проверен
-    
-    for(let i=0; i<9; i++){    
-     columnArr[i]=arr[i][col];
-    }
-  columnZeros=etalonArray.diff(columnArr); //массив нулей столбцов, проверен
-    
-  for(let i=0; i<9; i++){ 
-    for(let j=0; j<9; j++){
-      if(squareNumber(row,col)==squareNumber(i,j)){square.push(arr[i][j]);}
-  }}
-  squareZeros=etalonArray.diff(square); //массив нулей квадратов
- 
-  function squareNumber(a,b){return Math.floor(b/3)+Math.floor(a/3)*3;} //номер квадрата, проверен
-  
-  return rowZeros.analog(columnZeros).analog(squareZeros);//поиск совпадений в массиве нулей рядов, столбцов и квадратов, проверен
-  
-  
+
 function zeroCell(arr){
   for(let i=0; i<9; i++){ 
     for(let j=0; j<9; j++){
@@ -65,4 +34,53 @@ function zeroCell(arr){
   }
   return false;
 }
-}    
+
+
+function potencialValues(row,col,arr){
+  
+function analog(a,b){
+    let result=[];
+    for(let item of a){
+      if(b.indexOf(item)>=0){result.push(item);}
+    }
+    return result;
+  }  
+
+function diff(a,b){
+    let result=[];
+    for(let item of a){
+      if(b.indexOf(item)<0){result.push(item);}
+    }
+    return result;
+  }  
+
+
+
+
+    let rowZeros=[];
+    let etalonArray=[1,2,3,4,5,6,7,8,9];//эталонный массив
+    let columnArr=[];
+    let columnZeros=[];
+    let square=[];
+    let squareZeros=[];
+    
+    rowZeros=diff(etalonArray,arr[row]);//массив нулей рядов, проверен
+   
+    for(let i=0; i<9; i++){    
+     columnArr[i]=arr[i][col];
+    }
+  columnZeros=diff(etalonArray,columnArr); //массив нулей столбцов, 
+  
+
+    
+  for(let i=0; i<9; i++){ 
+    for(let j=0; j<9; j++){
+      if(squareNumber(row,col)==squareNumber(i,j)){square.push(arr[i][j]);}
+  }}
+  squareZeros=diff(etalonArray,square); //массив нулей квадратов
+ 
+  function squareNumber(a,b){return Math.floor(b/3)+Math.floor(a/3)*3;} //номер квадрата, проверен
+  
+  return analog(squareZeros,analog(columnZeros,rowZeros));//поиск совпадений в массиве нулей рядов, столбцов и квадратов, проверен
+}
+
